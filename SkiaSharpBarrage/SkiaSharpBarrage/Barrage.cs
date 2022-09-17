85ab43456585aa1cc372ba51ab4b96d5c3ec91f0
闫驚鏵（Jinhua  Yan） <yanjinhuamail@gmail.com>
2022-09-17 22:04:27 +0800

    add in SkiaSharpBarrage
    Use SkiaSharp to realize the bullet screen
    
    Co-Authored-By: kesshei <6630768+kesshei@users.noreply.github.com>

---
+++ SkiaSharpBarrage/SkiaSharpBarrage/Barrage.cs
@@ -0,0 +1,72 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SkiaSharpBarrage
+{
+    public class Barrage
+    {
+        private List<MsgInfo> _MsgInfo;
+        private int _num, _index;
+        private double _right, _top;
+        private float _width, _height;
+        private SKTypeface _font;
+
+        public Barrage(SKTypeface font,float width,float height, List<string> strList)
+        {
+            _width = width;
+            _height = height;  
+            _font = font;
+            _num = (int)height / 40;
+            _MsgInfo = new List<MsgInfo>();
+            foreach (var item in strList)
+            {
+                BuildMsgInfo(item);
+            }
+        }
+
+        void BuildMsgInfo(string text)
+        {
+            _index++;
+            if (_right != 0)
+                _width = (float)_right;
+            var model = new MsgInfo(text, _font, _width);
+            _right = _right == 0 ? _height + model.Width : _right;
+            var y = _height - 40;
+            _top = _top + 40 >= y ? 40 : _top;
+            model.Y = (float)_top;
+            _MsgInfo.Add(model);
+            _top += 60;
+        }
+        public void AddBarrage(string text)
+        {
+            BuildMsgInfo(text);
+        }
+
+        public void Render(SKCanvas canvas, SKTypeface font, int width, int height, List<string> strList)
+        {
+            for (int i = 0; i < _MsgInfo.Count; i++)
+            {
+                var info = _MsgInfo[i];
+                var guid = string.Empty;
+                info.Move(out guid);
+                if (!string.IsNullOrEmpty(guid))
+                {
+                    var model = _MsgInfo.FirstOrDefault(x => x.GUID == guid);
+                    _MsgInfo.Remove(model);
+
+                }
+                canvas.DrawImage(info.SKImage, info.X, info.Y);
+            }
+           
+        }
+    }
+}
---
+++ SkiaSharpBarrage/SkiaSharpBarrage/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SkiaSharpBarrage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private SKTypeface _font;
+        private Barrage _barrage;
+        private  List<string> list = new List<string>();
+        public MainWindow()
+        {
+            list.Add("2333");
+            list.Add("测试弹幕公众号：WPF开发者");
+            list.Add("很难开心");
+            list.Add("LOL~");
+            list.Add("青春记忆");
+            list.Add("bing");
+            list.Add("Microsoft");
+            InitializeComponent();
+            var index = SKFontManager.Default.FontFamilies.ToList().IndexOf("微软雅黑");
+            _font = SKFontManager.Default.GetFontStyles(index).CreateTypeface(0);
+            _barrage = new Barrage(_font,(float)Width, (float)Height - (float)MyGrid.ActualHeight, list);
+            skElement.PaintSurface += SkElement_PaintSurface;
+            Loaded += delegate
+            {
+                myMediaElement.Source = new Uri(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Leagueoflegends.mp4"));
+            };
+            _ = Task.Run(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            skElement.InvalidateVisual();
+                        });
+                        _ = SpinWait.SpinUntil(() => false, 1000 / 60);//每秒60帧
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            });
+        }
+
+
+
+        private void SkElement_PaintSurface(object? sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+            canvas.Clear();
+            _barrage.Render(canvas, _font, e.Info.Width, e.Info.Height, list);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _barrage.AddBarrage(tbBarrage.Text);
+        }
+    }
+}
